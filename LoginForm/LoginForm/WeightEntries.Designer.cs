ef91cc91123435e48c1ca080edcaa498929e1f6e
saqib-azhar <saqibabdullahazhar@gmail.com>
2019-04-08 00:08:45 +0500

    First Commit

---
+++ LoginForm/LoginForm/WeightEntries.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace LoginForm
+{
+    partial class WeightEntries
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 24);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(1131, 400);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Products";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(905, 290);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(657, 445);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 41);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Quit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(398, 445);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 41);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Dashboard";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(968, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(968, 289);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 66);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(968, 96);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 36);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(963, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 28);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select Product";
+            // 
+            // WeightEntries
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1161, 517);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Name = "WeightEntries";
+            this.Text = "Weight Entries";
+            this.Load += new System.EventHandler(this.WeightEntries_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ LoginForm/LoginForm/WeightEntries.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class WeightEntries : Form
+    {
+        String cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\saqib\Desktop\LoginForm\LoginForm\Database1.mdf;Integrated Security=True";
+        public WeightEntries()
+        {
+            InitializeComponent();
+            fillProductsComboBox();
+            loadAllWeightEntries();
+        }
+
+        private void WeightEntries_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            loadAllWeightEntries();
+        }
+        private void loadAllWeightEntries()
+        {
+            SqlConnection myConnection = default(SqlConnection);
+            myConnection = new SqlConnection(cs);
+            myConnection.Open();
+            SqlCommand myCommand = default(SqlCommand);
+
+            myCommand = new SqlCommand("SELECT Product_Name as [Product Name], Weight, Created_By as [Created By],Created_At as [Created At], Loose_Weight [Is Loose Weight] FROM WeightEntries", myConnection);
+            DataTable dt = new DataTable();
+            SqlDataAdapter sdr = new SqlDataAdapter(myCommand);
+            sdr.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            myConnection.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MainForm uf = new MainForm();
+            uf.ShowDialog();
+        }
+
+        private void fillProductsComboBox()
+        {
+            SqlConnection myConnection = default(SqlConnection);
+            myConnection = new SqlConnection(cs);
+            SqlCommand myCommand = default(SqlCommand);
+
+            myCommand = new SqlCommand("SELECT * FROM Products", myConnection);
+
+            SqlDataReader myReader;
+            try
+            {
+                myConnection.Open();
+                myReader = myCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    string prodName = myReader.GetString(1);
+                    comboBox1.Items.Add(prodName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            myConnection.Close();
+        }
+    }
+}
